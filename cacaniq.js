f63e0e8a185097f4304d3648fdadc8fc1b5f6a79
Eymael0 <eymafilho@gmail.com>
2026-08-31 09:51:16 -0300

    31/08

---
+++ cacaniq.js
@@ -0,0 +1,19 @@
+const simbolos = [
+    "img/red-apple_1f34e.png",
+    "img/trofeu.png",
+    "img/diam.png",
+    "img/glowing-star_1f31f.png",
+]
+    
+    const containerSlot1 = document.getElementById('slot1');
+    const containerSlot2 = document.getElementById('slot2');
+    const containerSlot3 = document.getElementById('slot3');
+    const elementoMensagem = document.getElementById ('mensagem');
+
+
+    botaGirar.addEventListener('click', function() {
+        const indice1 = Math.floor(Math.random() * simbolos.lenght);
+      
+        
+
+    })
